f2c4c2bac33c318370e0b369aec1922508333b4c
JeeviteshJuneja <jeeviinca@gmail.com>
2017-12-06 15:51:58 -0600

    Corrected Cart view

--- app/src/main/java/com/example/ui_functional_prototype/pay.java
+++ app/src/main/java/com/example/ui_functional_prototype/pay.java
@@ -20,7 +20,7 @@ public class pay extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
     public void pay(View view) {
-        Intent intent = new Intent(this, MainMenu.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
     public void back(View view) {
